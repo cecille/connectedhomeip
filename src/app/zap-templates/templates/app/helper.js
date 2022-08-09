a32705a57a341bcd2b668c2457d766d9bef5bccf
Cecille Freeman <cecille@google.com>
2022-08-09 17:44:59 -0400

    check2

--- src/app/zap-templates/templates/app/helper.js
+++ src/app/zap-templates/templates/app/helper.js
@@ -868,31 +868,39 @@ async function GetPixitType(type) {
   return basicType;
 }
 
-async function PixitInC (val, type) {
+async function PixitInC(val, type) {
   const basicType = await GetPixitType.call(this, type);
   return "mPixit[\"" + val + "\"].Get<" + basicType + ">()"
 }
 
-async function formatPixit(val, type, typeNamespace)
-{
+async function formatPixit(val, type, useSpans) {
   const pixitInC = await PixitInC.call(this, val, type);
   if (StringHelper.isOctetString(type)) {
-    // This will currently do weird things if the pixit is a hex string. So...don't do that.
-    return "chip::ByteSpan(chip::Uint8::from_const_char(" + pixitInC + ".c_str()), " + pixitInC + ".length())";
+    if (useSpans) {
+      // This will currently do weird things if the pixit is a hex string. So...don't do that.
+      return "chip::ByteSpan(chip::Uint8::from_const_char(" + pixitInC + ".c_str()), " + pixitInC + ".length())";
+    } else {
+      return pixitInC + ".c_str()";
+    }
   } else if (StringHelper.isCharString(type)) {
-    // This should be the UTF-8 encode length, but it's just the length. So, don't use weird chars.
-    return "chip::CharSpan(" + pixitInC + ".c_str(), " + pixitInC + ".length())";
-  }else {
+    if (useSpans) {
+      // This should be the UTF-8 encode length, but it's just the length. So, don't use weird chars.
+      return "chip::CharSpan(" + pixitInC + ".c_str(), " + pixitInC + ".length())";
+    } else {
+      return pixitInC + ".c_str()";
+    }
+  } else {
     return pixitInC;
   }
 }
 
-async function maybeSubPixit(val, type, typed)
-{
+async function maybeSubPixit(val, type, typedLiteral, useSpans) {
   if (isPixit(val)) {
-    return  await formatPixit.call(this, val, type);
-  } else if (typed) {
-    return  await asTypedLiteral.call(this, val, type);
+    return await formatPixit.call(this, val, type, useSpans);
+  } else if (typedLiteral) {
+    return await asTypedLiteral.call(this, val, type);
+  } else if (StringHelper.isOctetString(type) || StringHelper.isCharString(type)) {
+    return "\"" + val + "\"";
   } else {
     return val;
   }
