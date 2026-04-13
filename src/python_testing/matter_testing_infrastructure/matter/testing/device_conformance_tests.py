c5f8207e128883a9932e1c29d3627c045f991fbc
cecille <cecille@google.com>
2026-04-13 18:32:05 -0400

    Remove in-progress things that are no longer in progress

--- src/python_testing/matter_testing_infrastructure/matter/testing/device_conformance_tests.py
+++ src/python_testing/matter_testing_infrastructure/matter/testing/device_conformance_tests.py
@@ -123,10 +123,7 @@ class DeviceConformanceTests(BasicCompositionTests):
         ignore_attributes: dict[int, list[int]] = {}
         if ignore_in_progress_test_event_only_disallowed_for_certification:
             # This is a manually curated list of attributes that are in-progress in the SDK, but have landed in the spec
-            in_progress_attributes = {
-                Clusters.ThreadNetworkDiagnostics.id: [0x3F, 0x40],
-                Clusters.BasicInformation.id: [0x18],
-            }
+            in_progress_attributes = {}
             ignore_attributes.update(in_progress_attributes)
 
         if is_ci:
@@ -145,7 +142,7 @@ class DeviceConformanceTests(BasicCompositionTests):
         if is_ci:
             # This is a manually curated list of features that are present in the SDK, but not in the spec.
             # TODO(#43606): Remove the AUX feature (0x04) from here once spec updates are merged to include the feature.
-            ci_ignore_features = {Clusters.AccessControl.id: [0x04]}
+            ci_ignore_features = {}
             ignore_feature_masks.update(ci_ignore_features)
 
         success = True
